70d80aa3955bf75312595e997b2e8e9b54bbae19
Rauan de Santana Rosa <rauanoronha@hotmail.com>
2023-06-09 20:35:40 -0300

    Commit Inicial

---
+++ android/app/src/main/kotlin/com/pastoralfamiliar/institutocana/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pastoralfamiliar.institutocana
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:institutocana/paginas/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instituto Caná',
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const HomePage(),
+      },
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          brightness: Brightness.light,
+          backgroundColor: Colors.blueGrey.shade50,
+          primarySwatch: Colors.green,
+          accentColor: Colors.green.shade300,
+          cardColor: Colors.green.shade100,
+          errorColor: Colors.red,
+        ),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
