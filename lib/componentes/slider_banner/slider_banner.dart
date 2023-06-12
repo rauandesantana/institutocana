381a7c4c5735c63649b324ad3f108b2bed68d31d
Rauan de Santana Rosa <rauanoronha@hotmail.com>
2023-06-12 16:38:11 -0300

    Corrigindo Componente SliderBanner

--- lib/componentes/slider_banner/slider_banner.dart
+++ lib/componentes/slider_banner/slider_banner.dart
@@ -1,44 +1,50 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 typedef SliderBuilder = Widget Function(BuildContext, int, Size);
 
 class SliderBanner extends StatelessWidget {
-  final Widget? title;
-  final double? height;
   final bool? gridMode;
+  final Size sliderSize;
   final Size itemSize;
   final EdgeInsetsGeometry? itemMargin;
   final int itemCount;
   final SliderBuilder itemBuilder;
+  final ScrollController? controller;
+  final ScrollPhysics? physics;
 
   const SliderBanner({
     super.key,
-    this.title,
-    this.height,
     this.gridMode,
+    required this.sliderSize,
     required this.itemSize,
     this.itemMargin,
     required this.itemCount,
     required this.itemBuilder,
+    this.controller,
+    this.physics,
   });
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    int count = (width ~/ itemSize.width).toInt();
+    final isMobile = (!kIsWeb) ? Platform.isAndroid || Platform.isIOS : false;
+    int count = (sliderSize.width ~/ itemSize.width).toInt();
     if (count < 1) count = 1;
     final gridLayout = SliverGridDelegateWithFixedCrossAxisCount(
       crossAxisCount: count,
       mainAxisExtent: itemSize.height,
     );
-    final gridSize = Size(
-      (width - ((itemMargin?.horizontal ?? 0) * count)) / count,
+    final itemGridSize = Size(
+      (sliderSize.width - ((itemMargin?.horizontal ?? 0) * count)) / count,
       itemSize.height,
     );
 
     final slide = SizedBox(
       height: itemSize.height,
       child: ListView.builder(
+        controller: controller,
+        physics: physics,
         scrollDirection: Axis.horizontal,
         itemCount: itemCount,
         itemBuilder: (context, index) => Padding(
@@ -49,13 +55,15 @@ class SliderBanner extends StatelessWidget {
     );
 
     final grid = SizedBox(
-      height: height,
+      height: sliderSize.height,
       child: GridView.builder(
+        controller: controller,
+        physics: physics,
         scrollDirection: Axis.vertical,
         itemCount: itemCount,
         itemBuilder: (context, index) => Padding(
           padding: itemMargin ?? EdgeInsets.zero,
-          child: itemBuilder(context, index, gridSize),
+          child: itemBuilder(context, index, itemGridSize),
         ),
         gridDelegate: gridLayout,
       ),
@@ -63,12 +71,7 @@ class SliderBanner extends StatelessWidget {
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        (title != null)
-            ? Padding(padding: const EdgeInsets.only(left: 25), child: title!)
-            : const SizedBox(width: 0, height: 0),
-        (gridMode == true) ? grid : slide,
-      ],
+      children: <Widget>[(gridMode == true && !isMobile) ? grid : slide],
     );
   }
 }
--- lib/paginas/home.dart
+++ lib/paginas/home.dart
@@ -16,11 +16,70 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool get isMobile => (!kIsWeb) ? Platform.isAndroid || Platform.isIOS : false;
+  ScrollController scrollView = ScrollController();
+  ScrollController sliderBanner = ScrollController();
+  bool toggleScroll = false;
+
+  @override
+  void initState() {
+    toggleScroll = false;
+    scrollView.addListener(() {
+      if (!isMobile) {
+        final scrollMax = scrollView.position.maxScrollExtent;
+        final scrollCurrent = scrollView.position.pixels;
+        if (scrollCurrent >= scrollMax) setState(() => toggleScroll = true);
+      }
+    });
+    sliderBanner.addListener(() {
+      if (!isMobile) {
+        final scrollCurrent = sliderBanner.position.pixels;
+        if (scrollCurrent == 0.0) setState(() => toggleScroll = false);
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollView.dispose();
+    sliderBanner.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final isMobile = (!kIsWeb) ? (Platform.isAndroid || Platform.isIOS) : false;
-    final size = MediaQuery.of(context).size;
-    final smallMode = size.width < 450;
+    final smallMode = MediaQuery.of(context).size.width < 450;
+
+    ScrollPhysics? scrollPhysics({bool? invert}) {
+      if (isMobile) return null;
+      bool toggle = (invert == true) ? !toggleScroll : toggleScroll;
+      return (toggle) ? const NeverScrollableScrollPhysics() : null;
+    }
+
+    Widget itemCursos(
+      BuildContext context,
+      int index,
+      Size size,
+      Map<String, dynamic> data,
+    ) {
+      return ItemBannerCursos(
+        size: size,
+        urlImage:
+            "https://images.pexels.com/photos/459403/pexels-photo-459403.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "Lorem ipsum dolor sit",
+        objective: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+            "Suspendisse sit amet turpis elit. Etiam eget tempor turpis, vitae "
+            "lacinia massa. Morbi at lobortis magna, ut malesuada erat. Integer "
+            "eget molestie erat, a finibus mi. Proin eu mi eu nibh molestie porttitor."
+            " In libero ipsum, blandit cursus neque quis, varius malesuada nunc.",
+        date: "10/06/2023",
+        local: "Instituto Caná para Família",
+        onTap: () {
+          print(index);
+        },
+      );
+    }
 
     return Scaffold(
       appBar: AppBarCustom(
@@ -43,45 +102,40 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: size.width,
-          child: Column(
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(bottom: 25),
-                child: BannerHome(),
-              ),
-              SliderBanner(
-                title: TextTitle(context: context, title: "Cursos"),
-                height: 400,
-                gridMode: !isMobile,
-                itemSize: const Size(400, 200),
-                itemMargin: const EdgeInsets.all(5),
-                itemCount: 300,
-                itemBuilder: (context, index, size) {
-                  return ItemBannerCursos(
-                    size: size,
-                    urlImage:
-                        "https://images.pexels.com/photos/459403/pexels-photo-459403.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-                    title: "Lorem ipsum dolor sit",
-                    objective:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-                        "Suspendisse sit amet turpis elit. Etiam eget tempor turpis, vitae "
-                        "lacinia massa. Morbi at lobortis magna, ut malesuada erat. Integer "
-                        "eget molestie erat, a finibus mi. Proin eu mi eu nibh molestie porttitor."
-                        " In libero ipsum, blandit cursus neque quis, varius malesuada nunc.",
-                    date: "10/06/2023",
-                    local: "Instituto Caná para Família",
-                    onTap: () {
-                      print(index);
-                    },
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          final size = Size(constraints.maxWidth, constraints.maxHeight - 54);
+
+          return SingleChildScrollView(
+            controller: scrollView,
+            physics: scrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 25),
+                  child: BannerHome(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: TextTitle(context: context, title: "Cursos"),
+                ),
+                SliderBanner(
+                  sliderSize: size,
+                  controller: sliderBanner,
+                  physics: scrollPhysics(invert: true),
+                  gridMode: true,
+                  itemSize: const Size(400, 200),
+                  itemMargin: const EdgeInsets.all(5),
+                  itemCount: 300,
+                  itemBuilder: (context, index, size) {
+                    return itemCursos(context, index, size, {});
+                  },
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
